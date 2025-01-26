167afbb2632ebe4669897fe554c60d21ba5e5598
Leonel Marcos <mfjleonel@gmail.com>
2025-01-26 08:24:59 -0300

    fix: .env.example

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getQuizzes } from "../store/quizzesSlice";
+
 export default function Main() {
+  const dispatch = useDispatch();
+  const quizzes = useSelector((state) => state.quizzes);
+
+  useEffect(() => {
+    dispatch(getQuizzes());
+  }, []);
+
+  useEffect(() => {
+    console.log(quizzes);
+  }, [quizzes]);
+
   console.log(process.env.REACT_APP_API_URL);
 
   return <div>Testando!</div>;
